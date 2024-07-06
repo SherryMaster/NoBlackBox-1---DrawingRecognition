d62ba2832910ee103dfdd4c7691c40b60e2122f1
SherryUnity <128972759+SherryMaster@users.noreply.github.com>
2024-07-06 17:36:35 +0500

    added format number function

--- web/chart/math.js
+++ web/chart/math.js
@@ -4,6 +4,10 @@ math.lerp = (a, b, t) => {
     return a + (b - a) * t
 }
 
+math.formatNumber = (n, dec) => {
+    return n.toFixed(dec)
+}
+
 if (typeof module !== 'undefined') {
     module.exports = math
 }
