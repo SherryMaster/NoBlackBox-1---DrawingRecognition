072dcb5d6313e94ceee7bad32fe0480dd579d86e
SherryUnity <128972759+SherryMaster@users.noreply.github.com>
2024-07-07 11:17:09 +0500

    implemented private methods for Chart class

--- web/chart/chart.js
+++ web/chart/chart.js
@@ -11,5 +11,72 @@ class Chart{
         this.canvas.style = `background-color: white;`
 
         container.appendChild(this.canvas)
+
+        this.ctx = this.canvas.getContext("2d")
+        this.transparency = 0.5
+
+        this.pixelBounds = this.#getPixelBounds()
+        this.dataBounds = this.#getDataBounds()
+
+        this.#draw()
+    }
+
+    #getPixelBounds(){
+        const {canvas, margin} = this;
+        const bounds = {
+            left: margin,
+            right: canvas.width - margin,
+            top: margin,
+            bottom: canvas.height - margin
+        }
+
+        return bounds
+    }
+
+    #getDataBounds(){
+        const {samples} = this
+        const x = samples.map(sample=>sample.point[0])
+        const y = samples.map(sample=>sample.point[1])
+
+        const minX = Math.min(...x)
+        const maxX = Math.max(...x)
+        const minY = Math.min(...y)
+        const maxY = Math.max(...y)
+
+        const bounds = {
+            left: minX,
+            right: maxX,
+            top: maxY,
+            bottom: minY
+        }
+
+        return bounds
+    }
+
+    #draw(){
+        const {ctx, canvas} = this;
+
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.globalAlpha = this.transparency
+        this.#drawPoints()
+        ctx.globalAlpha = 1
+    }
+
+    #drawPoints(){
+        const {ctx, samples, dataBounds, pixelBounds} = this
+
+        for (const sample of samples){
+            const {point, label} = sample
+            const x = point[0]
+            const y = point[1]
+
+            const pixelLoc = [
+                math.remap(dataBounds.left, dataBounds.right, pixelBounds.left, pixelBounds.right, x),
+                math.remap(dataBounds.top, dataBounds.bottom, pixelBounds.top, pixelBounds.bottom, y)
+            ]
+
+            graphics.drawPoint(ctx, pixelLoc)
+
+        }
     }
 }
