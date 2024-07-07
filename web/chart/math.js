963457b42f532fa16c791a325dae94a4bef55b7a
SherryUnity <128972759+SherryMaster@users.noreply.github.com>
2024-07-07 10:16:47 +0500

    implemented remap

--- web/chart/math.js
+++ web/chart/math.js
@@ -1,11 +1,15 @@
 const math = {}
 
-math.Lerp = (a, b, t) => {
+math.lerp = (a, b, t) => {
     return a + (b - a) * t
 }
 
 math.invLerp = (a, b, v) => {
     return (v - a) / (b - a)
+}
+
+math.remap = (oldA, oldB, newA, newB, v) => {
+    return math.lerp(newA, newB, math.invLerp(oldA, oldB, v))
 }
 
 math.formatNumber = (n, dec=0) => {
